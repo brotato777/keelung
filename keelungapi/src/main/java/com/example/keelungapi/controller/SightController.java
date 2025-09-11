f7346913d1049a5a8f73f61c450590c33ae5433e
brotato777 <123862938+brotato777@users.noreply.github.com>
2025-09-11 11:22:01 +0800

    ap

--- keelungapi/src/main/java/com/example/keelungapi/controller/SightController.java
+++ keelungapi/src/main/java/com/example/keelungapi/controller/SightController.java
@@ -1,19 +1,14 @@
 package com.example.keelungapi.controller;
 
 import com.example.keelungapi.models.Sight;
-import com.example.keelungapi.repository.SightRepository;
 import com.example.keelungapi.service.SightService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.net.URI;
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import jakarta.annotation.PostConstruct;
 
@@ -22,12 +17,10 @@ import jakarta.annotation.PostConstruct;
 @RequestMapping("/SightAPI")
 public class SightController {
 
-    private final SightRepository sightRepository;
     private final SightService sightService;
     private static final Logger logger = LoggerFactory.getLogger(SightController.class);
 
-    public SightController(SightRepository sightRepository, SightService sightService) {
-        this.sightRepository = sightRepository;
+    public SightController(SightService sightService) {
         this.sightService = sightService;
     }
 
@@ -40,7 +33,7 @@ public class SightController {
     @GetMapping
     public List<Sight> getSightsByWeb(@RequestParam String zone) {
         logger.info("從網路爬取 {} 區的景點", zone);
-        return sightService.getSightsByZone(zone);
+        return sightService.getSightsByWeb(zone);
     }
 
     @GetMapping("/db")
@@ -54,24 +47,5 @@ public class SightController {
         logger.info("Crawling all Sights");
         sightService.crawlAllSights();
         return ResponseEntity.ok("Crawling all Sights completed");
-    }
-
-    @PostMapping
-    public ResponseEntity<Void> createSight(@RequestBody Sight sight) {
-        sight.setId(null);
-        sightRepository.insert(sight);
-
-        URI uri = ServletUriComponentsBuilder
-                .fromCurrentRequestUri()
-                .path("/{id}")
-                .build(Map.of("id", sight.getId()));
-
-        return ResponseEntity.created(uri).build();
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<Sight> getSight(@PathVariable String id) {
-        Optional<Sight> sightOp = sightRepository.findById(id);
-        return sightOp.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
